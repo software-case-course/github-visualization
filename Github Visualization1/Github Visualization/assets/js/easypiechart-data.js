b02ce5b0b26c26afe5d2a0401328436abd469254
sharontaozi <499400402@qq.com>
2017-05-11 08:47:33 +0800

    前端代码

---
+++ Github Visualization1/Github Visualization/assets/js/easypiechart-data.js
@@ -0,0 +1,55 @@
+$(function() {
+    $('#easypiechart-teal').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-orange').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-red').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-blue').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+ 
+ $(function() {
+    $('#easypiechart-eal').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-range').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+
+$(function() {
+    $('#easypiechart-ed').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-lue').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
