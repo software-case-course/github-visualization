9aaa289b5fe1cf215e5f7dabec0bc913f3a2ba1a
LiQiQin <503787826@qq.com>
2017-05-14 19:31:17 +0800

    定时获取数据
    每天0点定时调用函数获取数据

--- myTimer.py
+++ myTimer.py
@@ -1,10 +1,15 @@
+# -*- coding: cp936 -*-
 import time
 from datetime import datetime
 import Save_language
 
 def getdata():
+    #获取项目语言情况
     language = Save_language()
     language.Get_language()
+    #获取每年项目数量情况
+    repositories = Save_repositories()
+    repositories.Get_repositories
     
 if __name__ == "__main__":
     while True:
